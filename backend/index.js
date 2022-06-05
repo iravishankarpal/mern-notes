5e3ed4d5eef99f1c081f3f6d15f779024e642d57
iravishankarpal <iravishankarpal@gmail.com>
2022-06-05 16:08:08 +0530

    inetail commit and compled started server

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+// const { response } = require("express");
+const express = require("express");
+const data = require("./data");
+const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
+app.get("/", (req, res) => {
+  res.send(data);
+});
+
+app.get("/:id", (req, res) => {
+  const note = data.find((x) => x._id === Number(req.params.id));
+  console.log(req.params);
+  res.send(note);
+});
+const port = process.env.PORT || 5000;
+app.listen(port, console.log(` server is reunning  ${port}`));
